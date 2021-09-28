798d195458383aef4f721893c2dfd103f2b834cd
Agustin Vicario <agustingaspar@hotmail.com>
2021-09-28 11:49:50 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+//Console
+console.log("Benvenuto al Il Piccoli");
+//Entrada de datos
+let entrada = prompt("Ingrese su nombre");
+//Salida de datos
+let salida = "Benvenuto al Il Piccoli" + entrada;
+alert(salida);
