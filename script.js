3d4fd4310edc5dd12bcbfbf4e011be0d836defab
Agustin Vicario <agustingaspar@hotmail.com>
2021-10-14 12:51:31 -0300

    Desafio 5

--- script.js
+++ script.js
@@ -47,3 +47,13 @@ let destino1 = new Destino("Buenos Aires", "1000", 500, 1500, 650, 500);
 destino1.mostrar();
 let destino2 = new Destino("Miami", 800, 1500, 1000, 800, 200);
 destino2.mostrar();
+
+let destino3 = new Destino(
+  prompt("ingrese el destino"),
+  prompt("ingrese el valor presupuesto"),
+  prompt("ingrese el valor del alojamiento"),
+  prompt("ingrese el valor del transporte"),
+  prompt("ingrese el valor del comida"),
+  prompt("ingrese el valor de otro gastos")
+);
+console.log(destino3);
