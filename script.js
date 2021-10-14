15d82d18a64782e6e25bc03978c37114208bdd8d
Agustin Vicario <agustingaspar@hotmail.com>
2021-10-14 12:12:04 -0300

    Agregado del html, css, e implementacion del desafio 5

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
-function ingresarEntrada() {
-  let precio = parseInt(prompt("ingrese precio "));
-  return precio;
+class Destino {
+  constructor(
+    destino,
+    presupuesto,
+    alojamiento,
+    transporte,
+    comida,
+    otrosgastos
+  ) {
+    this.destino = destino;
+    this.presupuesto = presupuesto;
+    this.alojamiento = alojamiento;
+    this.transporte = transporte;
+    this.comida = comida;
+    this.otrosgastos = otrosgastos;
+  }
+  mostrar() {
+    console.log(
+      "Tu viaje es a" +
+        " " +
+        this.destino +
+        "," +
+        "tu presupuesto es de" +
+        " " +
+        this.presupuesto +
+        "," +
+        "tu gasato en alojamiento es de" +
+        " " +
+        this.alojamiento +
+        "," +
+        "tu gasto en trasporte es de" +
+        " " +
+        this.transporte +
+        "," +
+        "tu gasto en comida es de" +
+        " " +
+        this.comida +
+        "," +
+        "respecto a otros gastos el valor es de" +
+        " " +
+        this.otrosgastos +
+        "."
+    );
+  }
 }
-
-function procesarEntrada(numero) {
-  let iva = numero * 1.21;
-  return iva;
-}
-
-function mostrarSalida(valorFinal) {
-  alert(valorFinal);
-}
-
-let entrada = ingresarEntrada();
-let entradaModificada = procesarEntrada(entrada);
-mostrarSalida(entradaModificada);
+let destino1 = new Destino("Buenos Aires", "1000", 500, 1500, 650, 500);
+destino1.mostrar();
+let destino2 = new Destino("Miami", 800, 1500, 1000, 800, 200);
+destino2.mostrar();
